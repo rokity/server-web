1c55874f1e8d1a7e4f7083986b365a6e552b3656
rokity <riccardo.amadio1@gmail.com>
2018-10-26 18:42:57 +0200

    test di nuovo che il deploy funzioni

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 
 http.createServer(function (req, res) {
-  res.write('Test deploy work Hello World Dev Branch!'); 
+  res.write('Hello World Dev Branch!'); 
   res.end(); 
 }).listen(process.argv[3],process.argv[2]);
