bd17752e6e20a3bd69263082725c27efb18460de
rokity <riccardo.amadio1@gmail.com>
2018-10-30 12:57:35 +0100

    alias test

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('Hello World Master Branch With Hook Inside Git Server!'); 
+  res.write('Hello World Master Branch With Alias!'); 
   res.end(); 
 }).listen(process.argv[3],process.argv[2]);
